f93b91bae9ed722e9b0a94c5d929ee3aebb9f2d6
[Alexander Perera] <[s3721805@student.rmit.edu.au]>
2022-09-09 16:18:15 +1000

    THIRD PUSH no features implemented yet

--- src/main/java/com/example/test/controller/controller.java
+++ src/main/java/com/example/test/controller/controller.java
@@ -1,4 +1,37 @@
 package com.example.test.controller;
 
+import com.example.test.dao.doa;
+import com.example.test.model.Item;
+import com.example.test.model.Items;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class controller {
+
+    @RestController
+    @RequestMapping(path = "/items")
+    public class controller{
+        @Autowired
+        private dao itemDao;
+
+        @GetMapping(path="/item{id})
+
+        }
+
+        @GetMapping(path="/item"){
+            public Item
+                }
+
+
+        
+
+
+
+
+
+
+    }
 }
--- src/main/java/com/example/test/dao/doa.java
+++ src/main/java/com/example/test/dao/doa.java
@@ -4,7 +4,7 @@ package com.example.test.dao;
 import com.example.test.model.Item;
 import org.springframework.stereotype.Repository;
 
-import com.example.test.model.Items
+import com.example.test.model.Items;
 
 @Repository
 public class doa {
@@ -12,10 +12,10 @@ public class doa {
     private static Items list = new Items();
 
     static {
-        list.getItemList().add(new Item(1, "can", "description1", 50));
-        list.getItemList().add(new Item(2, "can", "description2", 70));
-        list.getItemList().add(new Item(3, "can", "description3", 80));
-        list.getItemList().add(new Item(4, "can", "description4", 90));
+        list.getItemList().add(new Item("1", "can", "description1", 50));
+        list.getItemList().add(new Item("2", "can", "description2", 70));
+        list.getItemList().add(new Item("3", "can", "description3", 80));
+        list.getItemList().add(new Item("4", "can", "description4", 90));
     }
 
     public Items getAllItems()
